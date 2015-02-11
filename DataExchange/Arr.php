ca221b269c2055fea67bb0288a6c6b6759d0ca98
Adam Dornak <adamd@xcroco.com>
2015-02-11 14:42:13 +0100

    Netreba nastavovat nezmenene sloupce

--- DataExchange/Arr.php
+++ DataExchange/Arr.php
@@ -11,7 +11,7 @@ class Arr extends AExchange implements IExchange
 	public function load($data)
 	{
 		foreach ($data as $property => $value) {
-			if (!isset($this->excluded[$property]) && $this->orm->propertyExists($property)) {
+			if (!isset($this->excluded[$property]) && $this->orm->propertyExists($property) && ($value !== $this->orm->{$property})) {
 				$this->orm->{$property} = $value;
 			}
 		}
